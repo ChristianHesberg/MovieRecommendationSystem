3d0effd5c0f316da9cb1761e4e0bcbc9b4ba3b78
ChristianHesberg <chri59kx@easv365.dk>
2021-11-09 11:19:22 +0100

    fresh

--- src/movierecsys/gui/model/ListOfMoviesModel.java
+++ src/movierecsys/gui/model/ListOfMoviesModel.java
@@ -4,16 +4,30 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import movierecsys.be.Movie;
 import movierecsys.bll.OwsLogicFacade;
+import movierecsys.dal.MovieDAO;
+
+import java.io.IOException;
 
 public class ListOfMoviesModel
 {
 
     private ObservableList<Movie> allMovies;
     private OwsLogicFacade logicFacade;
+    private MovieDAO movieData;
 
     public ListOfMoviesModel()
     {
+        /*
         allMovies = FXCollections.observableArrayList();
+        movieData = new MovieDAO();
+        try {
+            allMovies.addAll(movieData.getAllMovies());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+         */
+
     }
 
     public ObservableList<Movie> getAllMovies() {
